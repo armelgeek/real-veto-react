c29b009e624ac0ccd3175f71d0c3da79139f535c
armelgeek <57889510+armelgeek@users.noreply.github.com>
2023-03-04 06:35:15 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL(`file://${path.join(__dirname, 'index.html')}`);
+}
+
+app.on('ready', createWindow);
